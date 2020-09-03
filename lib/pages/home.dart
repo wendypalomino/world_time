1dad81f2861569d1c02fad587dbfe52aebb2cc5b
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-09-03 19:05:22 -0400

    adding background

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,20 +6,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  // Create an empty map so when we build Home widget we can use the arguments
+  Map data = {};
   @override
   Widget build(BuildContext context) {
+
+    data = ModalRoute.of(context).settings.arguments;
+    print(data);
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: [
-              FlatButton.icon(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/choose_location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120,0,0),
+            child: Column(
+              children: [
+                FlatButton.icon(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/choose_location');
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text('Edit Location'),
+                ),
+                SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Text(
+                  data['location'],
+                  style: TextStyle(
+                    fontSize: 28,
+                    letterSpacing: 2.0,
+                  ),
+                )
+              ],),
+                SizedBox(height: 20),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66,
+                  ),
+                ),
+              ],
+            ),
           )),
     );
   }
